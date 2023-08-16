4a1553da82666d99c17ab20a293c444a00ea3fa6
Iaroslav Derkach <141680960+JaDerkach@users.noreply.github.com>
2023-08-16 16:27:15 +0300

    add to task typeof

--- JS Lesson-1,Task.js
+++ JS Lesson-1,Task.js
@@ -3,3 +3,12 @@ let middleName = `Yevgeniovich`
 let lastName = `Derkach`
 let person = `${firstName} ${middleName} ${lastName}`
 console.log(person)
+// =======================================
+
+let a = 100
+console.log(typeof a)
+let b = '100'
+console.log(typeof b)
+let c = true
+console.log(typeof c)
+// =================================
