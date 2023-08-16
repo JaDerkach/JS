4d0676addbf3cf851f2f5696705364240959c43c
Iaroslav Derkach <141680960+JaDerkach@users.noreply.github.com>
2023-08-16 16:45:33 +0300

    add to task number 1

--- JS Lesson-1,Task.js
+++ JS Lesson-1,Task.js
@@ -12,3 +12,27 @@ console.log(typeof b)
 let c = true
 console.log(typeof c)
 // =================================
+let a1='hello'
+console.log(a1)
+let a2='owu'
+console.log(a2)
+let a3='com'
+console.log(a3)
+let a4='ua'
+console.log(a4)
+let b1 = 1
+console.log(b1)
+let b2=10
+console.log(b2)
+let b3=-999
+console.log(b3)
+let b4=123
+console.log(b4)
+let c1=3.14
+console.log(c1)
+let c2=2.7
+console.log(c2)
+let d1= true
+console.log(d1)
+let d2=false
+console.log(d2)
